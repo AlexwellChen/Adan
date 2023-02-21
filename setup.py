8305dd3ac2a71c267b4a7d727f7e2a4841cce24e
Alexwell <935562189@qq.com>
2023-02-21 09:57:25 +0100

    Update setup.py
    Add args to install unfused version

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup
+from setuptools import setup, Command
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 from torch.cuda import is_available
+import sys
 
 cuda_extension = CUDAExtension(
             'fused_adan', 
             sources=['fused_adan/pybind_adan.cpp','./fused_adan/fused_adan_kernel.cu', './fused_adan/multi_tensor_adan_kernel.cu']
         )
+
+ext_list = [cuda_extension]
+
+if "--unfused" in sys.argv:
+    print("Building unfused version of adan")
+    ext_list = []
+    sys.argv.remove("--unfused")
 
 setup(
     name='adan',
@@ -21,6 +29,6 @@ setup(
         'Xie, Xingyu and Zhou, Pan and Li, Huan and '
         'Lin, Zhouchen and Yan, Shuicheng'
     ),
-    ext_modules=[cuda_extension] if is_available() else [],
+    ext_modules=ext_list if is_available() else [],
     cmdclass={'build_ext': BuildExtension} if is_available() else {},
 )

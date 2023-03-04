7fe3e9c5983071560f5c2526d27399f015462cee
Alexwell <935562189@qq.com>
2023-03-04 22:04:16 +0100

    Update scripts.sh

--- micro_benchmark/scripts.sh
+++ micro_benchmark/scripts.sh
@@ -9,3 +9,15 @@ python adan_micro_benchmark.py --foreach True --adaptive True --adaptive_ratio 0
 python adan_micro_benchmark.py --foreach True --adaptive True --adaptive_ratio 1.0 --log_name Multiple_tensors_with_adaptive_ratio_10
 # Multiple tensors with adaptive, ratio = 0.25
 python adan_micro_benchmark.py --foreach True --adaptive True --adaptive_ratio 0.25 --log_name Multiple_tensors_with_adaptive_ratio_025
+
+# Single tensor fused
+python adan_micro_benchmark.py --foreach False --fused True --log_name Single_tensor_fused
+# Multiple tensors without adaptive fused
+python adan_micro_benchmark.py --foreach True --fused True --log_name Multiple_tensors_without_adaptive_fused
+# Multiple tensors with adaptive fused, ratio = 0.5
+python adan_micro_benchmark.py --foreach True --fused True --adaptive True --adaptive_ratio 0.5 --log_name Multiple_tensors_with_adaptive_ratio_05_fused
+# Multiple tensors with adaptive fused, ratio = 1.0
+python adan_micro_benchmark.py --foreach True --fused True --adaptive True --adaptive_ratio 1.0 --log_name Multiple_tensors_with_adaptive_ratio_10_fused
+# Multiple tensors with adaptive fused, ratio = 0.25
+python adan_micro_benchmark.py --foreach True --fused True --adaptive True --adaptive_ratio 0.25 --log_name Multiple_tensors_with_adaptive_ratio_025_fused
+--------------benchmarking end----------------

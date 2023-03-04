7e717341706bcfe91e539a9fb418d5c2213bedcb
Alexwell <935562189@qq.com>
2023-03-04 20:06:13 +0100

    Update

---
+++ micro_benchmark/adan_micro_benchmark.py
@@ -0,0 +1,33 @@
+import torch
+import sys
+sys.path.append('..')
+from adan import Adan
+import argparse
+from torch.profiler import profile, record_function, ProfilerActivity
+
+def get_fake_parameters(n_params=10, size=512):
+    params = []
+    for i in range(n_params):
+        tensor = torch.randn(size, size, requires_grad=True, device='cpu')
+        tensor.grad = torch.randn(size, size, device='cpu')
+        params.append(tensor)
+    return params
+
+if __name__ == "__main__":
+    prof = torch.profiler.profile(
+            schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/adan_micro_benchmark'),
+            record_shapes=True,
+            profile_memory=True,
+            with_stack=True
+        )
+
+    params = get_fake_parameters(size=4096, n_params=100)
+    optimizer = Adan(params=params, foreach=True, fused=False)
+    with prof:
+        for i in range(10):
+            optimizer.step()
+            prof.step()
+    
+    
+        
